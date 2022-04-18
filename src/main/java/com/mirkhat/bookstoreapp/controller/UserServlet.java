6061666a14386adfefc70d0343635a4c52724c87
Mirkhat Asen <asen.mirkhat@bk.ru>
2022-04-18 10:39:32 +0600

    User servlet commit

---
+++ src/main/java/com/mirkhat/bookstoreapp/controller/UserServlet.java
@@ -0,0 +1,42 @@
+package com.mirkhat.bookstoreapp.controller;
+
+import com.mirkhat.bookstoreapp.dao.UserDao;
+import com.mirkhat.bookstoreapp.model.User;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet(name = "UserServlet", value = "/UserServlet")
+public class UserServlet extends HttpServlet {
+
+    private UserDao userDao = new UserDao();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+
+        try {
+            userDao.registerUser(user);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        response.sendRedirect("");
+    }
+}
