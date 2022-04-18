c1f895e0aac52adf5fbed05368e24eb4c3c5e816
Mirkhat Asen <asen.mirkhat@bk.ru>
2022-04-18 10:26:05 +0600

    User dao commit

---
+++ src/main/java/com/mirkhat/bookstoreapp/dao/UserDao.java
@@ -0,0 +1,36 @@
+package com.mirkhat.bookstoreapp.dao;
+
+import com.mirkhat.bookstoreapp.model.User;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class UserDao {
+    public int registerUser(User user) throws ClassNotFoundException, SQLException {
+        String INSERT_USERS_SQL = "INSERT INTO user" +
+                " (id, username, password) VALUES " +
+                " (?, ?, ?);";
+
+        int result = 0;
+
+        Class.forName("com.mysql.jdbc.Driver");
+
+        try (Connection connection = DriverManager
+                .getConnection("jdbc:mysql://localhost:3306/book_store?useSSL=false", "root", "root");
+
+             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL)) {
+             preparedStatement.setInt(1, 1);
+             preparedStatement.setString(2, user.getUsername());
+             preparedStatement.setString(3, user.getPassword());
+
+            System.out.println(preparedStatement);
+
+            result = preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
